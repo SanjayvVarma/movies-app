0037b5ecbe7de66fdfc008678898a6ef24d71cd7
SanjayvVarma <spverma.sanjay56056@gmail.com>
2024-07-23 20:14:32 +0530

    update

--- src/App.js
+++ src/App.js
@@ -29,9 +29,9 @@ function App() {
       .then((res) => {
         console.log(res);
         const url = {
-          backdrop: res.images.secure_base_url + "original",
-          poster: res.images.secure_base_url + "original",
-          profile: res.images.secure_base_url + "original"
+          backdrop: res.images?.secure_base_url + "original",
+          poster: res.images?.secure_base_url + "original",
+          profile: res.images?.secure_base_url + "original"
         }
         dispatch(getApiConfiguration(url))
       })
